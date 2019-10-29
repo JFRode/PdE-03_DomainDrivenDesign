1001701c1cceb63415d9dfd1d4d202d3bc59dccd
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-10-29 15:41:21 -0300

    CM - EFCore configured

---
+++ CustomerHub.Data/Context/CustomerHubDbContext.cs
@@ -0,0 +1,32 @@
+﻿using CustomerHub.Domain.Customer;
+using CustomerHub.Domain.User;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+
+namespace CustomerHub.Data.Context
+{
+    public class CustomerHubDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+
+        public CustomerHubDbContext(DbContextOptions<CustomerHubDbContext> options) : base(options)
+        {
+        }
+
+        public CustomerHubDbContext()
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
