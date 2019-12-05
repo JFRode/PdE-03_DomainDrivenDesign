f27d55d615b3015e14993df9c4c808d264d98592
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-12-05 09:44:39 -0300

    CM - Mappers Profile added

---
+++ CustomerHub.Application/Mappers/DtoToDomainProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CustomerHub.Application.Dto;
+using CustomerHub.Domain.Customer;
+using CustomerHub.Domain.User;
+
+namespace CustomerHub.Application.Mappers
+{
+    public class DtoToDomainProfile : Profile
+    {
+        public DtoToDomainProfile()
+        {
+            CreateMap<UserDto, User>();
+            CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
