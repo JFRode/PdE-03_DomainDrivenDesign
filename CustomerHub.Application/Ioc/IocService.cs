816c82edc129b652d48f0188fcb126835f6bd0c1
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-12-05 09:44:04 -0300

    CM - IocService implemented

---
+++ CustomerHub.Application/Ioc/IocService.cs
@@ -0,0 +1,15 @@
+﻿using CustomerHub.Application.Services;
+using CustomerHub.Application.Services.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CustomerHub.Application.Ioc
+{
+    public static class IocService
+    {
+        public static void Register(IServiceCollection services)
+        {
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<ICustomerService, CustomerService>();
+        }
+    }
+}
