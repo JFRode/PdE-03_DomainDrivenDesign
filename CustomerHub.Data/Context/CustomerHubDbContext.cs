6bbc01d7eea5a8280d6b5036befe03ddce4b0435
João Felipe Gonçalves <joaofelipe.rode@gmail.com>
2019-12-09 10:22:30 -0300

    CM - Removed commentary

--- CustomerHub.Data/Context/CustomerHubDbContext.cs
+++ CustomerHub.Data/Context/CustomerHubDbContext.cs
@@ -18,15 +18,5 @@ namespace CustomerHub.Data.Context
         public CustomerHubDbContext()
         {
         }
-
-        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        //{
-        //    var config = new ConfigurationBuilder()
-        //        .SetBasePath(Directory.GetCurrentDirectory())
-        //        .AddJsonFile("appsettings.json")
-        //        .Build();
-
-        //    optionsBuilder.UseSqlServer(config.GetConnectionString("DefaultConnection"));
-        //}
     }
 }
